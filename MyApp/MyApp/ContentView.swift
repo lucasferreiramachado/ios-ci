ce622063cc0a9041e530796fecbe6e907e1849e5
Lucas Ferreira Machado <lucasferreiramachadopro@gmail.com>
2022-05-11 15:14:05 -0300

    Create ios app project.

---
+++ MyApp/MyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by Lucas Ferreira Machado on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyApp/MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Lucas Ferreira Machado on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
